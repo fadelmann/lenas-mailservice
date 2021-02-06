a5dd3fc06d3cc1b2645b7f16ab95000517b83a8e
Fabian <fabian.adelmann@gmail.com>
2021-02-06 14:43:01 +0100

    build app, configure heroku

--- index.js
+++ index.js
@@ -1,27 +1,62 @@
 const nodemailer = require('nodemailer');
+const Hapi = require('@hapi/hapi');
+const Joi = require('joi');
 
-async function main() {
-  const transporter = nodemailer.createTransport({
-    host: 'send.one.com',
-    port: 465,
-    secure: true,
-    tls: {
-      requireTLS: true,
+const PORT = process.env.PORT || 8080;
+const HOST = process.env.HOST || '0.0.0.0';
+
+const transporter = nodemailer.createTransport({
+  host: 'send.one.com',
+  port: 465,
+  secure: true,
+  tls: {
+    requireTLS: true,
+  },
+  auth: {
+    user: 'info@lenafranzisca.com',
+    pass: 'O$!rJdEX&lJA#gG4',
+  },
+});
+
+const init = async () => {
+  const server = Hapi.server({
+    port: PORT,
+    host: HOST,
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/send-mail',
+    config: {
+      payload: {
+        parse: true,
+      },
     },
-    auth: {
-      user: 'info@lenafranzisca.com',
-      pass: 'O$!rJdEX&lJA#gG4',
+    handler(request) {
+      const { name, email, about, message } = request.payload;
+
+      // send mail with defined transport object
+      return transporter.sendMail({
+        from: '"Lena Franzsika 👻" <info@lenafranzisca.com>', // sender address
+        to: 'fabian.adelmann@gmail.com', // list of receivers
+        subject: 'My Website', // Subject line
+        html: `
+From: ${name}
+Email: ${email}
+About: ${about}
+
+${message}
+        `,
+      });
     },
   });
 
-  // send mail with defined transport object
-  await transporter.sendMail({
-    from: '"Lena Franzsika 👻" <info@lenafranzisca.com>', // sender address
-    to: 'fabian.adelmann@gmail.com', // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world?', // plain text bodynid
-    html: '<b>Hello world?</b>', // html body
-  });
-}
+  await server.start();
+};
 
-main().catch(err => console.log(err));
+process.on('unhandledRejection', err => {
+  console.log(err);
+  process.exit(1);
+});
+
+init();
